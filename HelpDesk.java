140c53eeaf73ff16b91e01b7b399b03911812d2f
Joelle Lum <jlum@stuy.edu>
2017-04-20 08:37:19 -0400

    added HelpDesk and Ticket

---
+++ HelpDesk.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+public class HelpDesk{
+    private ArrayPriorityQueue _data;
+    private ArrayList services;
+}
---
+++ Ticket.java
@@ -0,0 +1,18 @@
+public class Ticket{
+    private int _ID;
+    public String descr;
+    private int priority;
+    private String name;
+
+    public int getID(){
+	return _ID;
+    }
+
+    public String getName(){
+	return name;
+    }
+
+    public int getPriority(){
+	return priority;
+    }
+}
