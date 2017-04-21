6bbdbc82e9bc1f24a6cbc9f5a4442e906846f0e7
m2000chan <mchan9@stuy.edu>
2017-04-21 08:41:11 -0400

    update HelpDesk

--- HelpDesk.java
+++ HelpDesk.java
@@ -1,5 +1,20 @@
 import java.util.ArrayList;
 public class HelpDesk{
     private ArrayPriorityQueue _data;
-    private ArrayList services;
+    private final ArrayList SERVICES; 
+
+    public HelpDesk(){
+	_data = new ArrayPriorityQueue();
+	SERVICES = new ArrayList();
+	SERVICES.add("1");
+	SERVICES.add("2");
+    }
+
+    public static void main(String[] args){
+	//Prompts user for Ticket info
+	//Creates new ticket with info
+	//Enters ticket into _data(takes time to fix)
+	//
+    }
+    
 }
